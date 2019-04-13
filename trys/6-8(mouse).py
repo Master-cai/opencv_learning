ce35884bdf5ad87d44911b94223d573ed3b9bfeb
Master-cai <719591339@qq.com>
2019-04-13 22:47:31 +0800

    rotate

--- trys/6-8(mouse).py
+++ trys/6-8(mouse).py
@@ -1,26 +1,29 @@
 import cv2
 
 
-
+rect_start = (0, 0)
+rect_end = (0, 0)
 
 
 def on_mouse(event, x, y, flags, param):
-    rect_start = (0, 0)
-    rect_end = (0, 0)
     # 鼠标左键按下，抬起，双击
     if event == cv2.EVENT_LBUTTONDOWN:
+        global rect_start
         rect_start = (x, y)
+        print(rect_start)
     elif event == cv2.EVENT_LBUTTONUP:
+        global rect_end
         rect_end = (x, y)
-    cv2.rectangle(img, rect_start, rect_end, (0, 255, 0), 2)
+        print(rect_end)
+
 
 img = cv2.imread("lena.jpg")
 cv2.namedWindow('test')
 cv2.setMouseCallback("test", on_mouse)
-
+cv2.rectangle(img, rect_start, rect_end, (0, 255, 0), 2)
+cv2.imshow("test", img)
 while (1):
     cv2.imshow("test", img)
-
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cv2.destroyAllWindows()
